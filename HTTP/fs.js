5f89d4486114a653faad31bffc13fbe9ea01e82b
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-08 23:57:09 +0500

    file descriptor topic clear

--- HTTP/fs.js
+++ HTTP/fs.js
@@ -1,5 +1,51 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.open('data.txt', 'a+', (err, fd) => {
-    console.log(fd);
-});
+// fs.open('data.txt', 'a+', (err, fd) => {
+//      console.log(fd);
+// });
+
+// try {
+//     const fd = fs.openSync('date.txt', 'a+');
+//     console.log(fd)
+// }
+// catch (err) {
+//     console.log(err);
+// }
+
+// const fs = require('fs/promises');
+// // Or we can use => const fs = require('fs').promises before v14
+// async function example() {
+//     let fileHandle;
+//     try {
+//         fileHandle = await fs.open('data.txt', 'r');
+//         //console.log(fileHandle.fd);
+//         console.log(await fileHandle.readFile({ encoding: 'utf-8'}));
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+//     finally {
+//         await fileHandle.close();
+//     }
+// }
+
+// example();
+
+// Just for practice
+
+// const fs = require("fs/promises");
+
+// async function practice() {
+// 	let fileHandle;
+// 	try {
+// 		fileHandle = await fs.open("data.txt", "r");
+// 		console.log(fileHandle.fd);
+// 		console.log(await fileHandle.readFile({ encoding: "utf8" }));
+// 	} catch (err) {
+// 		console.log(err);
+// 	} finally {
+// 		await fileHandle.close();
+// 	}
+// }
+
+// practice();
