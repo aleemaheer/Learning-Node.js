26bc86a7d8d622e0c5e716e8f44d8119d7c368b2
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-09 00:27:53 +0500

    file path, joining path, resolving path

--- HTTP/fs.js
+++ HTTP/fs.js
@@ -52,18 +52,45 @@
 
 // NODE JS FILE STATS
 
-const fs = require('fs/promises');
+// const fs = require('fs/promises');
 
-async function fileStat() {
-    try {
-        const stats = await fs.stat('data.txt', 'r');
-        console.log(stats.isFile()); // true
-        stats.isDirectory(); // false
-        stats.isSymbolicLink(); // false
-        console.log("Size: ", stats.size);
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
-fileStat();
+// async function fileStat() {
+//     try {
+//         const stats = await fs.stat('data.txt', 'r');
+//         console.log(stats.isFile()); // true
+//         stats.isDirectory(); // false
+//         stats.isSymbolicLink(); // false
+//         console.log("Size: ", stats.size);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// fileStat();
+
+// NODE JS FILE PATHS
+
+const path= require('path');
+
+const extName = path.extname('data.txt');
+const dirName = path.dirname('data.txt');
+const baseName = path.basename('data.txt');
+const fileName = path.basename('data.txt', path.extname('data.txt')); 
+console.log(extName)
+console.log(dirName)
+console.log(baseName)
+console.log(fileName);
+
+// Joining path
+const name = 'Joe';
+const joinedPath = path.join('/', 'users', name, 'notes.txt'); // /users/name/notes.txt
+
+console.log(joinedPath);
+
+// Resolving path
+const resolvedPath = path.resolve('data.txt');
+path.resolve('tmp', 'joe.txt'); // '/Users/joe/tmp/joe.txt' if run from my home folder
+path.resolve('/etc', 'joe.txt'); // '/etc/joe.txt'
+path.normalize('/users/joe/..//test.txt'); // '/users/test.txt'
+
+console.log(resolvedPath);
